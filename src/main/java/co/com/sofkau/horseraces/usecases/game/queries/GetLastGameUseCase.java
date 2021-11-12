84a2a3f34be959ff184d7fe77968c9751e8a1c25
Jutololla <julianangulop@gmail.com>
2021-11-12 14:28:37 -0500

    Consulta gameid del mas reciente

--- src/main/java/co/com/sofkau/horseraces/usecases/game/queries/GetLastGameUseCase.java
+++ src/main/java/co/com/sofkau/horseraces/usecases/game/queries/GetLastGameUseCase.java
@@ -5,7 +5,8 @@ import co.com.sofkau.horseraces.repositories.GameRepository;
 public class GetLastGameUseCase {
     public Object apply(GameRepository gameRepository) {
 
-        return gameRepository.findAll().get(0).getGameId();
+        var games= gameRepository.findAll();
+        return games.get(games.size()-1).getGameId();
 
     }
 }
